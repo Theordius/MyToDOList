14594cff2f8ce4df3df12676e8bfc53213df3c00
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-21 21:26:36 +0100

    Create TableViewController and adjust settings

--- MyToDOList/Controllers/TodoListViewController.swift
+++ MyToDOList/Controllers/TodoListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class TodoListViewController: UIViewController {
+class TodoListViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
