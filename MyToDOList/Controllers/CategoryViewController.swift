fe2a8125ad8d4b2d4fd17e75a29d23a70f0235c2
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-24 22:13:21 +0100

    Add CoreData relationships and create part of necessary methods in CategoryViewController anges. Lines starting
    Add CoreData relationship and creat part of necessary methods in CategoryViewControler.

--- MyToDOList/Controllers/CategoryViewController.swift
+++ MyToDOList/Controllers/CategoryViewController.swift
@@ -6,16 +6,45 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    let contex = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        
+        cell.textLabel?.text = categories[indexPath.row].name
+        
+        return cell
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
     }
     
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-    }
+   
+    
+    //MARK: - Table Delegate Methods
     
 }
