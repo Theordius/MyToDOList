53b4f6b00d3b3c04bce2a8148c2edb8c0e17d436
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-24 20:35:20 +0100

    Add CategoryViewController and create dependencies

---
+++ MyToDOList/Controllers/CategoryViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryViewController.swift
+//  MyToDOList
+//
+//  Created by Rafał Gęsior on 24/03/2022.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
