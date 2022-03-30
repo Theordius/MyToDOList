d401fac4fa0664fa6e7bfda10e805017bd4c3793
Theordius <87307483+Theordius@users.noreply.github.com>
2022-03-30 21:48:51 +0200

    Add aditional removeItem functionality

--- MyToDOList/Controllers/CategoryViewController.swift
+++ MyToDOList/Controllers/CategoryViewController.swift
@@ -139,7 +139,6 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
                     print("Error deleting category \(error)")
                 }
                 
-                tableView.reloadData()
             }
         }
         
@@ -147,6 +146,12 @@ extension CategoryViewController: SwipeTableViewCellDelegate {
         deleteAction.image = UIImage(named: "delete-icon")
         
         return [deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeOptions {
+        var options = SwipeOptions()
+        options.expansionStyle = .destructive
+        return options
     }
     
 }
